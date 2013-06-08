ea53e33fe7e804b64e8a8934ef7f88d12ca85e3a
Michael Claveria <mike.claveria@gmail.com>
2013-06-07 23:13:18 -1000

    first solution to challenge 005

---
+++ python_challenge_005.py
@@ -0,0 +1,42 @@
+#Michael Claveria
+#Python Challenge 005
+#http://www.pythonchallenge.com/pc/def/peak.html
+#peak hell
+
+#I had to read about .p files to figure out to use pickle to read them...
+#Does peak hell sound like pickle? 
+
+import urllib2,pickle
+
+url = 'http://www.pythonchallenge.com/pc/def/banner.p'
+data = pickle.load(urllib2.urlopen(url))
+
+#print data
+
+#The printed data looks like some sort of list pairs.
+#print out the pairs to see an ascii image with # signs
+for row in data:
+	print ''.join(map(lambda p: p[0]*p[1], row))
+
+#Output:
+              #####                                                                      ##### 
+               ####                                                                       #### 
+               ####                                                                       #### 
+               ####                                                                       #### 
+               ####                                                                       #### 
+               ####                                                                       #### 
+               ####                                                                       #### 
+               ####                                                                       #### 
+      ###      ####   ###         ###       #####   ###    #####   ###          ###       #### 
+   ###   ##    #### #######     ##  ###      #### #######   #### #######     ###  ###     #### 
+  ###     ###  #####    ####   ###   ####    #####    ####  #####    ####   ###     ###   #### 
+ ###           ####     ####   ###    ###    ####     ####  ####     ####  ###      ####  #### 
+ ###           ####     ####          ###    ####     ####  ####     ####  ###       ###  #### 
+####           ####     ####     ##   ###    ####     ####  ####     #### ####       ###  #### 
+####           ####     ####   ##########    ####     ####  ####     #### ##############  #### 
+####           ####     ####  ###    ####    ####     ####  ####     #### ####            #### 
+####           ####     #### ####     ###    ####     ####  ####     #### ####            #### 
+ ###           ####     #### ####     ###    ####     ####  ####     ####  ###            #### 
+  ###      ##  ####     ####  ###    ####    ####     ####  ####     ####   ###      ##   #### 
+   ###    ##   ####     ####   ###########   ####     ####  ####     ####    ###    ##    #### 
+      ###     ######    #####    ##    #### ######    ###########    #####      ###      ######
